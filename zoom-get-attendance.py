cb637459072d79233ddcc9b0ce1e10e899394600
Miles Exner <dizolivemint@gmail.com>
2021-05-28 08:56:36 -0700

    Update with program and campus

--- zoom-get-attendance.py
+++ zoom-get-attendance.py
@@ -5,19 +5,24 @@ import jwt
 from datetime import datetime, timedelta
 import time
 from typing import NamedTuple, Dict, Union, cast
+import smtplib
+from email.message import EmailMessage
+import argparse
 
 API_KEY_KEY = 'api_key'
 API_SECRET_KEY = 'api_secret'
 TOKEN = 'token'
 URL = 'url'
+EMAIL_USER = 'email_user'
+EMAIL_PASS = 'email_pass'
 
-Creds = NamedTuple('Creds', [('key', str), ('secret', str), ('url', str), ('token', str)])
+Creds = NamedTuple('Creds', [('key', str), ('secret', str), ('url', str), ('token', str), ('email_user', str), ('email_pass', str)])
 
 # Open Schedule CSV
 # ---Start---
-def read_csv():
+def read_csv(filename: str):
     meetings = []
-    with open('mdl-zoom-2020-420-901.csv', newline='') as csvfile:
+    with open(filename, newline='') as csvfile:
         meeting_reader = csv.reader(csvfile, dialect='excel', delimiter=',', quotechar='|')
         next(meeting_reader)
         for row in meeting_reader:
@@ -42,8 +47,10 @@ def read_credentails() -> Creds:
     secret = raw_creds[API_SECRET_KEY]
     url = raw_creds[URL]
     token = raw_creds[TOKEN]
+    email_user = raw_creds[EMAIL_USER]
+    email_pass = raw_creds[EMAIL_PASS]
     
-    return Creds(key, secret, url, token)
+    return Creds(key, secret, url, token, email_user, email_pass)
 
 def generate_jwt(key: str, secret: str) -> str:
     expiry = datetime.utcnow() + timedelta(minutes=15)
@@ -60,9 +67,9 @@ def json_to_dict(res) -> dict:
     return data
 # ---End---
 
-# Get Meeting Participants
+# Get Meeting
 # ---Start--- #
-def get_meeting_request(valid_jwt: str, meeting: dict) -> str:
+def get_meeting(valid_jwt: str, meeting: dict) -> str:
     data = {}
     conn = http.client.HTTPSConnection('api.zoom.us')
     headers = {
@@ -74,14 +81,38 @@ def get_meeting_request(valid_jwt: str, meeting: dict) -> str:
         res = conn.getresponse()
         data = json_to_dict(res)
     except Exception as x:
-        data = { 'uuid': f'Error: {x} occured on call to fetch user' }
-        return cast(str, data['uuid'])
+        data = { 'topic': f'Error: {x} occured on call to fetch user' }
+        return cast(str, data['topic'])
 
     # Error check if there is an issue finding the user
     if 'code' in data:
-        data = { 'uuid': f'Error: {data["message"]} occured on call to get meeting' }
+        data = { 'topic': f'Error: {data["message"]} occured on call to get meeting' }
 
-    return cast(str, data['uuid'])
+    return data
+# ---End---
+
+# Get Meeting Report
+# ---Start--- #
+def get_meeting_report(valid_jwt: str, meeting: dict) -> str:
+    data = {}
+    conn = http.client.HTTPSConnection('api.zoom.us')
+    headers = {
+		'authorization': f'Bearer {valid_jwt}',
+		'content-type': 'application/json'
+	}
+    try:
+        conn.request('GET', f'/v2/report/meetings/{meeting["id"]}', body=None, headers=headers)
+        res = conn.getresponse()
+        data = json_to_dict(res)
+    except Exception as x:
+        data = { 'topic': f'Error: {x} occured on call to fetch user' }
+        return cast(str, data['topic'])
+
+    # Error check if there is an issue finding the user
+    if 'code' in data:
+        data = { 'topic': f'Error: {data["message"]} occured on call to get meeting' }
+
+    return data
 # ---End---
 
 # Get Meeting Participants
@@ -107,6 +138,8 @@ def get_meeting_participants_report_request(valid_jwt: str, meeting: dict) -> st
     if 'code' in data:
         data = { 'participants': f'Error: {data["message"]} occured on call to get participants' }
 
+    # print('Participants response')
+    # print(data)
     return cast(str, data['participants'])
 # ---End---
 
@@ -116,11 +149,6 @@ def get_meeting_participants_report_request(valid_jwt: str, meeting: dict) -> st
 def fetch_course(course_id: str, url: str, token: str) -> str:
     conn = http.client.HTTPSConnection(url)
     function = 'core_course_get_courses'
-    body = { 
-            "options": {
-                "ids": [course_id]
-            }
-        }
     try:
         conn.request('GET', f'/webservice/rest/server.php?wstoken={token}&wsfunction={function}&moodlewsrestformat=json&options[ids][0]={course_id}', body=None, headers={})
         res = conn.getresponse()
@@ -133,7 +161,7 @@ def fetch_course(course_id: str, url: str, token: str) -> str:
         data = [{ 
                 'shortname': f'{data["message"]}'
                }]
-    return data[0]['shortname']
+    return data
 # ---End---
 
 # Fetch Course Users
@@ -141,9 +169,6 @@ def fetch_course(course_id: str, url: str, token: str) -> str:
 def fetch_course_users(course_id: str, url: str, token: str) -> str:
     conn = http.client.HTTPSConnection(url)
     function = 'core_enrol_get_enrolled_users'
-    body = { 
-            "courseid": course_id
-        }
     try:
         conn.request('GET', f'/webservice/rest/server.php?wstoken={token}&wsfunction={function}&moodlewsrestformat=json&courseid={course_id}', body=None, headers={})
         res = conn.getresponse()
@@ -214,13 +239,56 @@ def write_log(log_csv: list, out_filename: str) -> bool:
     return True
 # ---End---
 
+# Create and send email for the meeting
+def send_attendance(absentees: list, course_name: str, course_id: str, course_program: str, meeting_time: str, email_user: str, email_pass: str):
+    course_campus = 'San Diego'
+    if course_name.find('.NY') != -1:
+        course_campus = 'New York'
+    if course_name.find('.CH') != -1:
+        course_campus = 'Chicago'
+    
+    for user in absentees:
+        msg = EmailMessage()
+        msg.set_content(f'''Synchronous Course Student Attendance
+        First Name: {user["firstname"]}
+        Last Name:  {user["lastname"]}
+        Email: {user["email"]}
+        Course Name:  {course_name}
+        Course Link: https://elearning.pacificcollege.edu/course/view.php?id={course_id}
+        Reason: Did not attend Zoom class meeting
+        Meeting date/time: {meeting_time}
+        {course_campus}
+        {course_program}''')
+        msg['Subject'] = 'Synchronous Course Student Attendance'
+        msg['From'] = 'eLearning <elearning@pacificcollege.edu>'
+        msg['To'] = 'mexner@pacificcollege.edu'
+        # Send email
+        mailserver = smtplib.SMTP('smtp.office365.com',587)
+        mailserver.ehlo()
+        mailserver.starttls()
+        mailserver.login(email_user, email_pass)
+        #Adding a newline before the body text fixes the missing message body
+        mailserver.send_message(msg)
+        mailserver.quit()
+
+
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Backs up Zoom cloud recordings')
+    parser.add_argument(
+        '--file',
+        metavar='CSV_FILE',
+        required=True,
+        help='CSV file of meeting settings'
+    )
+    
+    args = parser.parse_args()
+    
     # Create JWT
-    key, secret, url, token = read_credentails()
+    key, secret, url, token, email_user, email_pass = read_credentails()
     new_jwt = generate_jwt(key, secret)
 
     # Read Schedule
-    meetings = read_csv()
+    meetings = read_csv(args.file)
     
     # Create a list and log to write to CSV
     lti_csv = []
@@ -228,9 +296,14 @@ if __name__ == '__main__':
     
     # Loop through schedule
     for body in meetings:
-        course_name = fetch_course(body['course_id'], url, token)
         enrolled_user = []
         meeting_attendee = []
+        
+        # Get course name
+        meeting = get_meeting_report(new_jwt, body)
+        meeting_time = meeting['start_time']
+        course_name = meeting['topic']
+
         # Get enrolled users from Moodle
         users = fetch_course_users(body['course_id'], url, token)
         
@@ -239,15 +312,28 @@ if __name__ == '__main__':
         print(f'Course ID: {body["course_id"]}')
         print('______________________')
         
+        course_data = fetch_course(body['course_id'], url, token)
+        course_program = course_data[0]['customfields'][0]['value']
+        print('Course Program')
+        print('-----------')
+        print(course_program)
+        print('-----------')
+        
+        mdl_uid = 0
+        mdl_user = []
         for user in users:
             print(f'Enrolled User: {user["email"]}')
+            print(f'Enrolled User First Name: {user["firstname"]}')
+            print(f'Enrolled User Last Name: {user["lastname"]}')
             enrolled_user.append(user["email"])
-        
+            mdl_user.append({ "id": mdl_uid, "email": user["email"], "firstname": user["firstname"], "lastname": user["lastname"] })
+            mdl_uid += 1
         
         
         # Get participants from Zoom Meeting
         participants = get_meeting_participants_report_request(new_jwt, body)
         for participant in participants:
+            print(participant)
             print(f'Meeting Participant: {participant["user_email"]}')
             # print(f'join_time: {participant["join_time"]}')
             # print(f'leave_time: {participant["leave_time"]}')
@@ -256,6 +342,25 @@ if __name__ == '__main__':
         # Compare emails and log students that did not attend
         filtered = [email for email in enrolled_user if email not in meeting_attendee]
         print(f'Absent: {filtered}')
+        absentees = []
+        for email in filtered:
+            # print('Absentee values:')
+            # print(enrolled_user.index(email))
+            # print(mdl_user[enrolled_user.index(email)])
+            absentees.append(mdl_user[enrolled_user.index(email)])
+            
+        # Skip, if the class has over 5 students and only one is present
+        skip_attendence = False
+        if len(meeting_attendee) < 3:
+            if len(enrolled_user) > 6:
+                skip_attendence = True
+        if len(filtered) < 1:
+            skip_attendance = True
+                
+        # Send attendance if no skip
+        if skip_attendence == False:
+            # continue
+            send_attendance(absentees, course_name, body["course_id"], course_program, meeting_time, email_user, email_pass)
         
         # Compare join and leave time to course start and end time?
         # Get course duration
